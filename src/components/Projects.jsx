b9648772fed3e6f6172e5eefdb5074827b74fa5a
Rusiru Kalendra De Silva <IT22927316@my.sliit.lk>
2025-02-25 04:22:20 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,8 +5,10 @@ import AnimatedTitle from './AnimatedTitle'
 const Projects = () => {
   return (
     <div id="projects" className='bg-black pb-52'>
+      <div className='container mx-auto px-3 md:px-10'>
       <AnimatedTitle title="Projects" containerClass="!text-white text-center"/>
       <AniProject />
+      </div>
     </div>
   )
 }
